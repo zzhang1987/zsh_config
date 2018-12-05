fb27333cec1d0706a942ba395c63b79a39f97bc9
zhen <zhen@zzhang.org>
2018-12-05 18:48:19 +0800

    configure for windows added

--- zshrc.zsh
+++ zshrc.zsh
@@ -5,15 +5,25 @@ source ~/.config/zshconfig/zsh-git-prompt/zshrc.sh
 source ~/.config/zshconfig/zpm/zpm.zsh 
 PROMPT='%B%m%~%b$(git_super_status) %# '
 
+fpath=(~/.config/zshconfig/zsh-completions/src $fpath)
 
 zpm load zpm-zsh/ls
 zpm load zpm-zsh/dircolors-material
 zpm load zpm-zsh/title
 zpm load zpm-zsh/ssh
-
+zpm load zsh-users/zsh-syntax-highlighting
+zpm load paraqles/zsh-plugin-ssh
 
 PROMPT_TITLE='$USER@$HOST:$PWD'
 
 ### keybindings
 bindkey '^[[1;5C' vi-forward-blank-word
 bindkey '^[[1;5D' vi-backward-blank-word
+
+alias ssh='/usr/bin/ssh'
+
+LS_COMMON="$LS_COMMON -I NTUSER.DAT\* -I ntuser.dat\*"
+LS_COMMON="$LS_COMMON -I ntuser.ini"
+
+test -n "$LS_COMMON" &&
+alias ls="ls $LS_COMMON"
