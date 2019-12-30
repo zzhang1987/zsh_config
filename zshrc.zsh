455939cd15aaf7573246cdcf08d0e4904330a664
zhen <zhen@zzhang.org>
2019-12-30 10:20:10 +0800

    updated to newest zpm

--- zshrc.zsh
+++ zshrc.zsh
@@ -15,17 +15,61 @@ PATH="/usr/local/opt/gnu-getopt/bin:/usr/local/opt/coreutils/libexec/gnubin:$PAT
 
 ### Core
 zpm if ssh     \
-  zpm-zsh/tmux \
+    zpm-zsh/tmux
 
-zpm load mafredri/zsh-async  
-zpm load zpm-zsh/ls
-zpm load zpm-zsh/dircolors-material
-zpm load zpm-zsh/title
-zpm load zpm-zsh/ssh
-zpm load zsh-users/zsh-syntax-highlighting
-zpm load paraqles/zsh-plugin-ssh
-zpm load zpm-zsh/colors
-# zpm load zpm-zsh/zsh-command-not-found
+
+zpm if-not ssh            \
+  zpm-zsh/tmux,apply:path \
+
+
+### Compatibility
+zpm if termux          \
+    zpm-zsh/termux,async \
+
+
+### 3party plugins
+zpm                                                \
+  zpm-zsh/core-config                              \
+  zpm-zsh/ignored-users,async                      \
+  zpm-zsh/check-deps                               \
+  zpm-zsh/minimal-theme                            \
+  zpm-zsh/ls,async                                 \
+  zpm-zsh/colorize,async                           \
+  zpm-zsh/ssh,async                                \
+  zpm-zsh/dot,async                                \
+  zpm-zsh/dircolors-material,async                 \
+  zpm-zsh/undollar,async                           \
+  zsh-users/zsh-completions,apply:fpath,fpath:/src \
+
+
+zpm if-not ssh                                                                         \
+  zpm-zsh/dropbox,async                                                                \
+  lukechilds/zsh-better-npm-completion,async                                           \
+  tj/git-extras,source:/etc/git-extras-completion.zsh,async                            \
+  horosgrisa/utils,apply:path                                                          \
+                                                                                       \
+  zpm-zsh/readers,async                                                                \
+  zpm-zsh/clipboard,async                                                              \
+  zpm-zsh/mysql-colorize,async                                                         \
+  zpm-zsh/zshmarks,async                                                               \
+  voronkovich/gitignore.plugin.zsh,async                                               \
+  zpm-zsh/autoenv,async                                                                \
+                                                                                       \
+  mdumitru/fancy-ctrl-z,async                                                          \
+  hlissner/zsh-autopair,async                                                          \
+  zthxxx/zsh-history-enquirer,async                                                    \
+  zsh-users/zsh-history-substring-search,source:zsh-history-substring-search.zsh,async \
+  zdharma/fast-syntax-highlighting,async                                               \
+  horosgrisa/zsh-autosuggestions,source:zsh-autosuggestions.zsh,async                  \
+
+
+zpm                           \
+  omz/pip,async               \
+  omz/extract,async           \
+  omz/command-not-found,async \
+  omz/wp-cli,async            \
+
+  
 
 source ~/.config/zshconfig/3rd/command-not-found.sh
 
